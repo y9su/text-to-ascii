d21c0b482fc4d582a46074b099fc1331a7e1b72a
yasu <101958049+y9su@users.noreply.github.com>
2022-03-19 21:18:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pyfiglet
 import time
 import keyboard
+import pyperclip
 
-text = pyfiglet.figlet_format(input('Enter text : '))
+
+print("Find Fonts at http://www.figlet.org/examples.html\n")
+time.sleep(1.5)
+
+font = input("Font: ")
+text = pyfiglet.figlet_format(input('Enter text: '), font=f"{font}")
 print(text)
+
+print("Copied to clipboard!")
+pyperclip.copy(f"{text}")
+
 time.sleep(5)
